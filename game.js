c24321dbd594e2511e8a06675aeb417f6d444d58
harith123586 <harithisyrafbinlukman@gmail.com>
2024-08-26 16:21:12 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED,
-getSnakeHead, snakeintersection } from './grid.js'
-import { update as updateFood, drawFood } from './food.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
@@ -10,26 +9,26 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
-     window.location = '/'
+      window.location = '/'
     }
     return
   }
 
-  
-window.requestAnimationFrame(main)
+
+  window.requestAnimationFrame(main)
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-  if (secondsSincelastRender < 1 / SNAKE_SPEED) return
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-  
-lastRenderTime = currentTime
 
-update()
-draw()
+  lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
 window.requestAnimationFrame(main)
 
-function uodate() {
+function update() {
   updateSnake()
   updateFood()
   checkDeath()
