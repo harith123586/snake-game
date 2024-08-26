57c775aa2268aa84a46c68495351d45a89c55c73
harith123586 <harithisyrafbinlukman@gmail.com>
2024-08-26 16:20:34 +0800

    Update food.js

--- food.js
+++ food.js
@@ -1,5 +1,5 @@
-import { onSnake, expandSnake } from '/snake.js'
-import{ randomGridPosition } from '/grid.js'
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
 let food = getRandomFoodPosition()
 const EXPANSION_RATE = 5
@@ -12,11 +12,11 @@ export function update() {
 }
 
 export function draw(gameBoard) {
-  const foodElement = document.creatorElement('div')
+  const foodElement = document.createElement('div')
   foodElement.style.gridRowStart = food.y
   foodElement.style.gridColumnStart = food.x
-  foodElement.classlist.add('food')
-  gameboard.appendChild(foodElement)
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
 }
 
 function getRandomFoodPosition() {
