f5b3c53438bcfc70232fe10006abfb3ecab722bc
harith123586 <harithisyrafbinlukman@gmail.com>
2024-08-26 16:21:50 +0800

    Update grid.js

--- grid.js
+++ grid.js
@@ -8,8 +8,8 @@ export function randomGridPosition() {
 }
 
 export function outsideGrid(position) {
-  return {
+  return (
     position.x < 1 || position.x > GRID_SIZE ||
-    position.y < 1 || position.y > GRID_SIZE 
-   }
+    position.y < 1 || position.y > GRID_SIZE
+  )
 }
